c07ed66473941327c14198ae679dca574ca8b80e
Sina Moradian <sinacsharp@gmail.com>
2023-01-28 05:00:35 +0330

    Refactor...

--- Game.h
+++ Game.h
@@ -1,16 +1,26 @@
 #pragma once
 #include "Player.h"
-#include <vector>
+#include <queue>
+#include <map>
 #include "Dice.h"
 
 class Game
 {
 private:
 	int boardLength;
+	queue<Player> Players;
+	map<int, int> Snakes, Ladders;
 public:
-	Game();
+	Game();//default constructor
 	int getCordinates();
+	queue<Player> getPlayers();
+	map<int, int> getSnakes();
+	map<int, int> getLadders();
 	void setCordinates(int rows, int columns);
-	void startTheGame(vector<Player> players, int boardDimension, Dice dice);
+	void insertPlayer(Player player);
+	void insertSnake(pair<int, int> snakeTailHead);
+	bool isSnakeExist(int tail, int head);
+
+		//void startTheGame(vector<Player> players, int boardDimension, Dice dice, );
 };
 
