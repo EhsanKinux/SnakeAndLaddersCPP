749682ee374b80305bb5718e7124b38356d9da92
Sina Moradian <sinacsharp@gmail.com>
2023-01-29 23:19:27 +0330

    Add Player properties and methods

--- Player.cpp
+++ Player.cpp
@@ -9,3 +9,43 @@ string Player::getName()
 {
 	return name;
 }
+
+int Player::getCurrentPos()
+{
+	return currentPos;
+}
+
+int Player::getPrevPos()
+{
+	return prevPos;
+}
+
+bool Player::getValidation()
+{
+	return isValid;
+}
+
+void Player::setValidation(bool validate)
+{
+	isValid = validate;
+}
+
+bool Player::checkFinished()
+{
+	return isFinished;
+}
+
+void Player::setFinished(bool finished)
+{
+	isFinished = finished;
+}
+
+void Player::setCurrentPos(int pos)
+{
+	currentPos = pos;
+}
+
+void Player::setPrevPos(int pos)
+{
+	prevPos = pos;
+}
--- Player.h
+++ Player.h
@@ -6,14 +6,18 @@ class Player
 {
 private:
 	string name;
-	int currentPos, prevPos, diceVal;
-	bool finished , notStarted;
+	int currentPos = 0, prevPos = 0, diceVal = 0;
+	bool isFinished = false , isValid = false;
 public:
 	void setName(string pName);
 	string getName();
-	/*int getCurrentPos();
+	int getCurrentPos();
 	int getPrevPos();
+	bool getValidation();
+	void setValidation(bool validate);
+	bool checkFinished();
+	void setFinished(bool finished);
 	void setCurrentPos(int pos);
-	void setPrevPos(int pos);*/
+	void setPrevPos(int pos);
 };
 
